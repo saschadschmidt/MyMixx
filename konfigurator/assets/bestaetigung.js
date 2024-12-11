1f33ee73acdcd157f3fadd8dbc3d5dd6d561e337
Sascha Schmidt <57413321+saschadschmidt@users.noreply.github.com>
2024-12-11 08:12:28 +0100

    20241211

--- konfigurator/assets/bestaetigung.js
+++ konfigurator/assets/bestaetigung.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
         let gesamtPreis = 0;
         orderData.cart.forEach(item => {
             const itemElement = document.createElement('li');
-            itemElement.textContent = `${item.name}: ${item.quantity}${item.unit} - ${item.price}€`;
+            itemElement.innerHTML = `<div class="item-container"><div class="item-name">${item.quantity}${item.unit} ${item.name}</div> <div class="item-price"> ${item.price}€</div></div>`;
             bestellungsItemsContainer.appendChild(itemElement);
             gesamtPreis += parseFloat(item.price);
         });
@@ -18,14 +18,14 @@ document.addEventListener("DOMContentLoaded", () => {
         bestellungsItemsContainer.appendChild(gesamtPreisElement);
 
         bestellungsDatenContainer.innerHTML = `
-            <li><strong>Name:</strong> ${orderData.name}</li>
-            <li><strong>E-Mail:</strong> ${orderData.email}</li>
-            <li><strong>Datum:</strong> ${orderData.datum}</li>
-            <li><strong>Uhrzeit:</strong> ab ${orderData.uhrzeit} Uhr</li>
+            <li class="item-container"><strong>Name:</strong> ${orderData.name}</li>
+            <li class="item-container"><strong>E-Mail:</strong> ${orderData.email}</li>
+            <li class="item-container"><strong>Datum:</strong> ${orderData.datum}</li>
+            <li class="item-container"><strong>Uhrzeit:</strong> ${orderData.uhrzeit} Uhr</li>
         `;
 
         // Löschen der Bestelldaten und des Warenkorbs aus dem LocalStorage
-        localStorage.removeItem('orderData');
-        localStorage.removeItem('cart');
+        /* localStorage.removeItem('orderData');
+        localStorage.removeItem('cart'); */
     }
 });
